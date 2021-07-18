4cce7a43ee7d6e8c1748b1e2db6a37d424b93568
Tianium <zhangjyr@gmail.com>
2021-07-17 20:13:43 -0400

    Add mock conn

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/mason-leap-lab/go-utils
 go 1.15
 
 require (
+	github.com/jordwest/mock-conn v0.0.0-20180617021051-4896c6bd1641
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/onsi/ginkgo v1.15.0
 	github.com/onsi/gomega v1.10.5
+	github.com/zhangjyr/hashmap v1.0.2
 )
---
+++ net/mock_test.go
@@ -0,0 +1,66 @@
+package net_test
+
+import (
+	"fmt"
+	"net"
+
+	mock "github.com/jordwest/mock-conn"
+
+	. "github.com/mason-leap-lab/go-utils/net"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	shortcutAddress = "shortcut:%d:%s"
+)
+
+var _ = Describe("Mock", func() {
+	newResponseConn := func() (net.Conn, net.Conn) {
+		conn := mock.NewConn()
+		return conn.Client, conn.Server
+	}
+
+	It("should mock connection works", func() {
+		buff := make([]byte, 1024)
+		client, server := newResponseConn()
+		// reqReader, reqWriter, _, _ := newResponseConn()
+		go func() {
+			n, err := client.Write([]byte("ping"))
+			Expect(n).To(Equal(4))
+			Expect(err).To(BeNil())
+		}()
+
+		read, err := server.Read(buff)
+		Expect(err).To(BeNil())
+		Expect(string(buff[:read])).To(Equal("ping"))
+
+		go func() {
+			n, err := server.Write([]byte("pong"))
+			Expect(n).To(Equal(4))
+			Expect(err).To(BeNil())
+		}()
+
+		read, err = client.Read(buff)
+		Expect(err).To(BeNil())
+		Expect(string(buff[:read])).To(Equal("pong"))
+	})
+
+	It("should Validate recognize shortcut", func() {
+		shortcut := InitShortcut()
+		ip := "10.23.4.5"
+		fullip := ip + ":6378"
+
+		addr, ok := shortcut.Validate(fmt.Sprintf(shortcutAddress, 1, ip))
+		Expect(ok).To(Equal(true))
+		Expect(addr).To(Equal(ip))
+
+		addr, ok = shortcut.Validate(fmt.Sprintf(shortcutAddress, 1, fullip))
+		Expect(ok).To(Equal(true))
+		Expect(addr).To(Equal(fullip))
+
+		addr, ok = shortcut.Validate(fullip)
+		Expect(ok).To(Equal(false))
+		Expect(addr).To(Equal(""))
+	})
+})
