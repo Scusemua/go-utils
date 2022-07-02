54605e8847e0a00e4643d33f5fd0ec7284e19bf1
Tianium <zhangjyr@gmail.com>
2022-07-01 20:09:25 -0400

    Add config package

---
+++ config/options_test.go
@@ -0,0 +1,140 @@
+package config_test
+
+import (
+	"flag"
+
+	"github.com/mason-leap-lab/go-utils/config"
+	"github.com/mason-leap-lab/go-utils/logger"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type MyConfig struct {
+	config.Options
+
+	Test bool   `name:"test" description:"Option \"test\"."`
+	Name string `name:"name" description:"Option \"name\"."`
+}
+
+type MyLoggerConfig struct {
+	config.LoggerOptions
+
+	Test bool `name:"test" description:"Option \"test\"."`
+}
+
+type MyCompositeConfig struct {
+	config.SeedOptions
+	Logger config.LoggerOptions
+}
+
+func checkFlagSet(flagSet *flag.FlagSet, err error) {
+	Expect(flagSet).NotTo(BeNil())
+	if err == config.ErrPrintUsage {
+		flagSet.PrintDefaults()
+	}
+}
+
+var _ = Describe("Options", func() {
+	BeforeEach(func() {
+		Expect(config.LogLevel).To(Equal(logger.LOG_LEVEL_INFO))
+	})
+
+	AfterEach(func() {
+		config.LogLevel = logger.LOG_LEVEL_INFO
+	})
+
+	It("should polyfill fills the uninitialized Options", func() {
+		Expect(config.Polyfill(config.NewOptions(), nil)).To(BeNil())
+
+		myConfig := &MyConfig{}
+		Expect(config.Polyfill(myConfig, nil)).To(BeNil())
+		Expect(myConfig.Options).NotTo(BeNil())
+
+		compsiteConfig := &MyLoggerConfig{}
+		Expect(config.Polyfill(compsiteConfig, nil)).To(BeNil())
+		Expect(compsiteConfig.Options).NotTo(BeNil())
+
+		Expect(config.Polyfill(MyConfig{}, nil)).To(Equal(config.ErrNonPointer))
+	})
+
+	It("should simple config customization works", func() {
+		cfg := &MyConfig{Options: config.NewOptions()}
+		flagSet, err := config.ValidateOptionsWithFlags(cfg, "-test")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(true))
+	})
+
+	It("should embeded config customization works", func() {
+		var cfg MyLoggerConfig
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-debug")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(false))
+		Expect(cfg.Debug).To(Equal(true))
+		Expect(config.LogLevel).To(Equal(logger.LOG_LEVEL_ALL))
+	})
+
+	It("should composite config has no conflict", func() {
+		var cfg MyCompositeConfig
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-debug", "-seed=123")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Logger.Debug).To(Equal(true))
+		Expect(config.LogLevel).To(Equal(logger.LOG_LEVEL_ALL))
+		Expect(cfg.Seed).To(Equal(int64(123)))
+	})
+
+	It("should yaml works for options", func() {
+		var cfg MyConfig
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-yaml=options_test.yml")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(true))
+	})
+
+	It("should yaml works for embeded options", func() {
+		var cfg MyLoggerConfig
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-yaml=options_test.yml")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(true))
+		Expect(cfg.Debug).To(Equal(true))
+		Expect(config.LogLevel).To(Equal(logger.LOG_LEVEL_ALL))
+	})
+
+	It("should yaml works for composite options", func() {
+		var cfg MyCompositeConfig
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-yaml=options_test.yml")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Logger.Debug).To(Equal(true))
+		Expect(config.LogLevel).To(Equal(logger.LOG_LEVEL_ALL))
+	})
+
+	It("should yaml override default values", func() {
+		cfg := MyConfig{Name: "test"}
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-yaml=options_test.yml")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(true))
+		Expect(cfg.Name).To(Equal("Tianium"))
+	})
+
+	It("should parameter override yaml", func() {
+		cfg := MyConfig{Name: "test"}
+		flagSet, err := config.ValidateOptionsWithFlags(&cfg, "-yaml=options_test.yml", "-name=Elle")
+		checkFlagSet(flagSet, err)
+
+		Expect(err).To(BeNil())
+		Expect(cfg.Test).To(Equal(true))
+		Expect(cfg.Name).To(Equal("Elle"))
+	})
+})
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/mason-leap-lab/go-utils
 go 1.15
 
 require (
+	github.com/gookit/config/v2 v2.1.2
 	github.com/jordwest/mock-conn v0.0.0-20180617021051-4896c6bd1641
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+	github.com/mitchellh/mapstructure v1.5.0
 	github.com/onsi/ginkgo v1.15.0
 	github.com/onsi/gomega v1.10.5
 	github.com/zhangjyr/hashmap v1.0.2
